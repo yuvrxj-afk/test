77a8f5a3ee9d75058ebda191bd3d651a7368b4fe
yuvrxj <yuvrajsisodiya7302@gmail,com>
2023-08-16 08:29:53 +0530

    socket connection  / mouse events

--- index.js
+++ index.js
@@ -1,18 +1,61 @@
-const express = require('express')
-const http = require('http')
-const socket = require('socket.io')
+// Import required modules
+const express = require("express");
+const http = require("http");
+const socket = require("socket.io");
 
-
+// Create an Express app instance
 const app = express();
-const server = http.createServer(app)
-const io = socket(server)
+// Create an HTTP server using the Express app
+const server = http.createServer(app);
+// Create a Socket.IO instance attached to the server
+const io = socket(server);
 
-app.use(express.static('public'))
+// Serve static files from the "public" directory
+app.use(express.static("public"));
 
-app.get('/',(req,res)=>{
-    res.send("<h1>On board in</h1>")
-})
+// Maintain an array of connected sockets
+let connections = [];
 
-server.listen(3000,()=>{
-    console.log("working")
-})
+// Handle socket connections
+io.on("connect", (socket) => {
+  // Add the connected socket to the array
+  connections.push(socket);
+  // Log when a socket connects, using its unique ID
+  console.log(`Socket ${socket.id} got connected`);
+
+  // Reusable function to broadcast events to all connected sockets except the sender
+  function broadcastEvent(eventName, socket, eventData) {
+    connections.forEach((con) => {
+      if (con.id !== socket.id) {
+        con.emit(eventName, eventData);
+      }
+    });
+  }
+
+  // Socket event listeners
+  socket.on("draw", (data) => {
+    broadcastEvent("ondraw", socket, { x: data.x, y: data.y });
+  });
+
+  socket.on("down", (data) => {
+    broadcastEvent("ondown", socket, { x: data.x, y: data.y });
+  });
+
+  // Handle socket disconnections
+  socket.on("disconnect", (reason) => {
+    // Remove the disconnected socket from the array
+    connections = connections.filter((con) => con.id !== socket.id);
+    // Log when a socket disconnects, using its unique ID
+    console.log(`Socket ${socket.id} got disconnected`);
+  });
+});
+
+// Define a route for the root URL
+app.get("/", (req, res) => {
+  res.send("<h1>On board in</h1>");
+});
+
+// Start the server and listen on port 5000
+server.listen(5000, () => {
+  console.log("Server is listening on port 5000");
+});
--- public/script.js
+++ public/script.js
@@ -1,2 +1,42 @@
-let canvas = document.getElementById('canvas')
-let context = canvas.getContext('2d')
+let canvas = document.getElementById("cnvs");
+canvas.width = 0.8 * window.innerWidth;
+canvas.height = 0.8 * window.innerHeight;
+
+var io = io.connect("http://localhost:5000");
+
+let context = canvas.getContext("2d");
+
+let x;
+let y;
+
+let mouseDown = false;
+
+window.onmousedown = (e) => {
+  context.moveTo(x, y);
+  io.emit("down", { x, y });
+  mouseDown = true;
+};
+
+window.onmouseup = (e) => {
+  mouseDown = false;
+};
+
+io.on("ondraw", ({ x, y }) => {
+  context.lineTo(x, y);
+  context.stroke();
+});
+
+io.on("ondown", ({ x, y }) => {
+  context.moveTo(x, y);
+});
+
+window.onmousemove = (e) => {
+  x = e.clientX;
+  y = e.clientY;
+  // console.log({x,y})
+  if (mouseDown) {
+    io.emit("draw", { x, y });
+    context.lineTo(x, y);
+    context.stroke();
+  }
+};
